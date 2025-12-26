fba1db438ece7007ba6d551c1e5291338a27b0e5
ToxicStarfall <mr.dumbosayshi@gmail.com>
2025-12-26 09:33:55 -0600

    Camera projectile tracking

--- game.gd
+++ game.gd
@@ -6,13 +6,18 @@ var World: Node2D
 
 var ammo := 3
 var current_weapon: Weapon
+var last_projectile: Projectile
 
 var gravity := Vector2(0.0, 980.0)
+
+var Camera: Camera2D
 
 
 
 func _ready() -> void:
 	World = get_tree().root.get_node("Main/World")
+	Camera = World.get_node("Camera2D")
+
 	UI = get_tree().root.get_node("Main/UI")
 
 	current_weapon = World.get_node("Weapon")
@@ -22,13 +27,14 @@ func _ready() -> void:
 
 func _on_weapon_fired(projectile: Projectile):
 	World.add_child(projectile)
+	last_projectile = projectile
+	Camera.reparent(last_projectile)
+	Camera.align()
+	Camera.position = Vector2.ZERO  # Re-center camera onto projectile
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("zoom_in"):
-		var zoom: Vector2 = World.get_node("Camera2D").zoom
-		World.get_node("Camera2D").zoom = (zoom + (Vector2.ONE * 0.01)).minf(2.0)
-		#World.get_node("Camera2D").zoom += Vector2.ONE * 0.01).minf(2.0)
+		Camera.zoom = (Camera.zoom + (Vector2.ONE * 0.01)).minf(2.0)
 	if event.is_action_pressed("zoom_out"):
-		var zoom: Vector2 = World.get_node("Camera2D").zoom
-		World.get_node("Camera2D").zoom = (zoom - (Vector2.ONE * 0.01)).maxf(0.01)
+		Camera.zoom = (Camera.zoom - (Vector2.ONE * 0.01)).maxf(0.01)
